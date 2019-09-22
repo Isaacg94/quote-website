c6c21e98b60e9d338fe0ad85c9e8119ab0f43928
Isaacg94 <7248zack@gmail.com>
2019-09-22 15:28:31 +0300

    generate custom dateAgo pipe as well as logic for elapsed time, also add bootstrap and popper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { LikeComponent } from './like/like.component';
+import { DateAgoPipe } from './pipes/date-ago.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailComponent,
-    LikeComponent
+    LikeComponent,
+    DateAgoPipe
   ],
   imports: [
     BrowserModule,
