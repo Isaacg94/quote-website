4372c9d361ba5fea1af5687b288dedbee21f0a49
Isaacg94 <7248zack@gmail.com>
2019-09-23 00:22:49 +0300

    add temporary changes to page layout and styling

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,9 +9,9 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-    new Quote(1, 'watch me', 'jaden smith', 'me',new Date(2018,10,29)),
-    new Quote(2,'indigo','willow smith', 'Google User',new Date(2019,8,12)),
-    new Quote(3,'get jiggy','snoop lion', '6ix9ine',new Date(2019,8,23)),
+    new Quote(1, 'Nothing valuable can be lost by taking time.', 'Abraham Lincoln', 'Obizzy',new Date(2018,10,29)),
+    new Quote(2,"Spiderman's real name is Peter Parker.", '6ix9ine','MemeLord',new Date(2019,8,12)),
+    new Quote(3,'To the moon & back, remember?','my Ex', 'Google User',new Date(2019,8,23)),
 
   ];
 
